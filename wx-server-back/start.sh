757554a5ce0ac48ef23eb02d0c1d004ffee96217
zhouli <1021907330@qq.com>
2022-07-20 18:38:04 +0800

    修改启动脚本

--- wx-server-back/start.sh
+++ wx-server-back/start.sh
@@ -1,10 +1,11 @@
 ﻿#bin/bash
 cd /data/zhouli/code/wx-bot-server/wx-server-back/
-git pull origin main
+git pull origin feature/desktop
 docker build -f HZY.WebHost/Dockerfile -t wx-bot-server  .
 docker rm -f wx-bot-back
-docker run -d  --name wx-bot-back \ 
--p 9901:9901 \
--v /data/zhouli/wx-bot-back/appsettings.Production.json:/app/HZY.WebHost/appsettings.Production.json \
+docker run -d --name wx-bot-back -p 9901:9901 \
+-v /data/zhouli/wx-bot-back/appsettings.Production.json:/app/appsettings.Production.json \
 -v /data/zhouli/wx-bot-back/applogs:/app/applogs \
 wx-bot-server
+
+
