c93477c1a0502c8f155196c3e058498902a6320c
zhouli <1021907330@qq.com>
2022-07-27 19:38:01 +0800

    修改邮件服务service

--- wx-server-back/HZY.Infrastructure/Email/EmailService.cs
+++ wx-server-back/HZY.Infrastructure/Email/EmailService.cs
@@ -12,21 +12,23 @@ namespace HZY.Infrastructure.Email
     /// </summary>
     public class EmailService
     {
+        private readonly string smtpServer;
+        private readonly int smtpPort;
+        private readonly bool enableSsl;
         private readonly string username;
-        private readonly SmtpClient smtpClient;
+        private readonly string password;
         public EmailService(string smtpServer,
             int smtpPort,
             bool enableSsl,
             string username,
             string password)
         {
+            this.smtpServer = smtpServer;
+            this.smtpPort = smtpPort;
+            this.enableSsl = enableSsl;
             this.username = username;
-            smtpClient = new SmtpClient();
-            smtpClient.EnableSsl = enableSsl;//由于使用了授权码必须设置该属性为true
-            smtpClient.DeliveryMethod = SmtpDeliveryMethod.Network;//指定电子邮件发送方式
-            smtpClient.Host = smtpServer; //指定SMTP服务器
-            smtpClient.UseDefaultCredentials = false; //不和请求一块发送
-            smtpClient.Credentials = new System.Net.NetworkCredential(username, password);//用户名和密码
+            this.password = password;
+
 
         }
 
@@ -38,18 +40,28 @@ namespace HZY.Infrastructure.Email
         /// <param name="body">邮件内容</param>
         public void SendEmail(string recipient, string subject, string body)
         {
+            var smtpClient = new SmtpClient
+            {
+                EnableSsl = enableSsl,//由于使用了授权码必须设置该属性为true
+                DeliveryMethod = SmtpDeliveryMethod.Network,//指定电子邮件发送方式
+                Host = smtpServer, //指定SMTP服务器
+                UseDefaultCredentials = false, //不和请求一块发送
+                Credentials = new System.Net.NetworkCredential(username, password)//用户名和密码
+            };
             // 发送人和收件人
-            MailMessage mailMessage = new(username, recipient);
-            //主题
-            mailMessage.Subject = subject;
-            //内容
-            mailMessage.Body = body;
-            //正文编码
-            mailMessage.BodyEncoding = Encoding.UTF8;
-            //设置为HTML格式
-            mailMessage.IsBodyHtml = true;
-            //优先级
-            mailMessage.Priority = MailPriority.Low;
+            MailMessage mailMessage = new(username, recipient)
+            {
+                //主题
+                Subject = subject,
+                //内容
+                Body = body,
+                //正文编码
+                BodyEncoding = Encoding.UTF8,
+                //设置为HTML格式
+                IsBodyHtml = true,
+                //优先级
+                Priority = MailPriority.Low
+            };
             // 发送邮件
             smtpClient.Send(mailMessage);
         }
