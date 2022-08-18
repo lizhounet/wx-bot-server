fe7829e6ec04c063c7d3de279119732770bc4161
zhouli <1021907330@qq.com>
2022-08-18 18:01:17 +0800

    [up]修改顶层await

--- wx-server-front/src/service/home/homeService.js
+++ wx-server-front/src/service/home/homeService.js
@@ -1,20 +1,21 @@
 import { post, get } from '@/scripts/request';
 import { useAppStore } from "@/store";
 const appStore = useAppStore();
-const userInfo= await appStore.getUserInfo();
 export default {
     /**
      * 首页状态数据
      * 
      * @param {*} bRefresh 
      */
-    getWxUserInfo(bRefresh) {
-        return get(`/admin/home/user-info/${userInfo.id}`, {bRefresh}, false);
+    async getWxUserInfo(bRefresh) {
+        const userInfo = await appStore.getUserInfo();
+        return get(`/admin/home/user-info/${userInfo.id}`, { bRefresh }, false);
     },
     /**
     * 获取登录二维码
     */
-    getLoginQrCode() {
-        return get(`/admin/home/login-qrcode/${userInfo.id}`,null, false);
+     async getLoginQrCode() {
+        const userInfo = await appStore.getUserInfo();
+        return get(`/admin/home/login-qrcode/${userInfo.id}`, null, false);
     },
 }
